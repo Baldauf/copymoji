815755a7f51a91c5f80a9b936ea026f00b70ae05
Shane Baldauf <shane.baldauf@gmail.com>
2016-10-21 08:27:57 -0600

    more people and things

--- js/main.js
+++ js/main.js
@@ -7,7 +7,21 @@ $(document).ready(function(){
       $(this).toggle( keyword.length < 1 || $(this).attr('data-models').indexOf(keyword) > -1 );
     });
   });
+  console.log('bye');
+
+$(document).on('scroll', function(e) {
+    var paag = $('#page');
+  console.log(paag.scrollTop);
+
+  // if (page.scrollTop > 147) {
+  //   console.log("scroll");
+  //   $('.page').addClass("fix-search");
+  // } else {
+  //   $('.page').removeClass("fix-search");
+  // }
+    
+});
 
   // make clipboard clip
   new Clipboard('.emoji__mojo');
-})
+});
