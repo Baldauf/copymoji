6bd9204289644036a6196375925be8cd987cc5d7
Shane Baldauf <shane.baldauf@gmail.com>
2016-10-27 16:38:39 -0600

    fix icons

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
 
   $('#search').keyup(function() {
-    var keyword = $(this).val().toLowerCase(),
-        cat = $('.category');
+    var keyword = $(this).val().toLowerCase();
 
-    if( !$(this).val() ) {
-      cat.removeClass('category--hide');
-    }
+    // rebuild on keystroke
+    $('.category').removeClass('category--hide');
 
+    // simple search toggle
     $('.emoji__mojo').each( function() {
       $(this).toggle( keyword.length < 1 || $(this).attr('data-models').indexOf(keyword) > -1 );
     });
 
+    // hide categories if no results
     $('.emoji').each( function() {
       if($(this).children(':visible').length == 0) {
         $(this).parent().addClass('category--hide');
@@ -27,21 +27,16 @@ $(document).ready(function(){
   var clipboard = new Clipboard('.emoji__mojo--copy');
 
   clipboard.on('success', function(e) {
-    function doThis(){
-      console.log('yo');
+    function hideCopy(){
       e.trigger.classList.remove('copied');
     }
 
     e.trigger.classList.add('copied');
-    setTimeout(doThis, 2000);
+    setTimeout(hideCopy, 2000);
     e.clearSelection();
   });
 
-  clipboard.on('error', function(e) {
-    e.trigger.classList.remove('copy-btn--copied');
-    e.trigger.classList.add('copy-btn--fail');
-  });
-
+  // smooth scroll to el
   $(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -49,7 +44,7 @@ $(document).ready(function(){
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
         if (target.length) {
           $('html, body').animate({
-            scrollTop: target.offset().top
+            scrollTop: target.offset().top - 129
           }, 1000);
           return false;
         }
@@ -60,18 +55,55 @@ $(document).ready(function(){
 
 $(window).scroll(function() {    
   var scroll = $(window).scrollTop(),
-      search = $(".search"),
-      filters = $(".filters");
+  search = $(".search"),
+  filters = $(".filters")
+  filter = $(".filter");
 
+  // lock search & jumpers
   if (scroll >= 173 && scroll < 220) {
     search.addClass("search--lock");
     filters.removeClass("filters--lock");
   }
   else if (scroll >= 220) {
-      filters.addClass("filters--lock");
-      search.addClass("search--lock");
+    filters.addClass("filters--lock");
+    search.addClass("search--lock");
   } else {
     filters.removeClass("filters--lock");
     search.removeClass("search--lock");
   }
+
+  filter.each( function(e){
+    var self = $(this),
+        target = $(this.hash),
+        text = target.children('.category__text'),
+        height = target.height(),
+        dist = target.offset().top - 130,
+        bottom = height + dist,
+        active = 'filter--active';
+
+        console.log(text)
+
+    if(scroll > dist && scroll < bottom) {
+      self.addClass(active);
+    } else {
+      self.removeClass(active)
+    }
+  });
+
+  // $(".filter--people").each( function(e){
+  //   var self = $(this),
+  //       target = $(this.hash),
+  //       height = target.height(),
+  //       dist = target.offset().top,
+  //       bottom = height + dist,
+  //       active = 'filter--active';
+
+  //       console.log(bottom)
+
+  //   if(scroll > dist && scroll < bottom) {
+  //     self.addClass(active);
+  //   } else {
+  //     self.removeClass(active)
+  //   }
+  // });
 });
